0422aba3b872e178b44cde8898503cdd529360d2
John Jannetty <jjannetty@gmail.com>
2020-05-03 16:41:14 -0400

    add level 4

--- level_4.js
+++ level_4.js
@@ -2,11 +2,26 @@ window.initLevel4 = () => {
   const lineStart = document.querySelector('.level-4-start');
   const lineEnd = document.querySelector('.level-4-end');
   const line = document.querySelector('.level-4-line-1');
-  var path = [];
 
-  lineStart.onmouseenter = () => {
-    line.onmousemove = (event) => {
-      console.log(`x: ${event.clientX}, y:${event.clientY}`);
-    }
+  lineStart.onmouseover = () => {
+    line.addEventListener('mousemove', handleMouseMove, true);
+  }
+
+  line.onmouseleave = () => {
+    line.removeEventListener('mousemove', handleMouseMove, true);
+    lineEnd.removeEventListener('mouseenter', handleEnd, true);
+  }
+
+  function handleEnd() {
+    setNextLevel(4);
+  }
+
+  function handleMouseMove(e) {
+    e = e || window.event;
+
+    console.log(`x: ${e.clientX}, y: ${e.clientY}`);
+
+    lineEnd.addEventListener('mouseenter', handleEnd, true);
   }
 }
+  
